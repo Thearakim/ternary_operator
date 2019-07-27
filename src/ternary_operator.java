d87946b6574ceb785b79da3e1a168f6c70932393
Theara Kim <45727527+Thearakim@users.noreply.github.com>
2019-07-27 11:15:15 +0700

    update
    update

--- src/ternary_operator.java
+++ src/ternary_operator.java
@@ -1,10 +1,10 @@
-class ternary_operator
+class ternary_operator  // declare java that it is a class 
 {
-	public static void main(String[] args)
+	public static void main(String[] args)  //every java must have main 
 	{
-		int leap_year = 28;
-		String result;
-		result = (leap_year == 29)?"is a leap year" : "is not a leap year";
-		System.out.println(result);
+		int leap_year = 28;  //declear leap_year is integer, it is statically_type
+		String result;  // result as String
+		result = (leap_year == 29)?"is a leap year" : "is not a leap year";  // reuslt = if leap_yeat = 29  true -> is a leap year but we declared equal 28 so it is false so display is not a leap year 
+		System.out.println(result); // print or display standard output of the result
 	}
 }
